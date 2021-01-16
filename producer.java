b78ddd5a59ca0f6575ee625099d3c95bf0570747
TIWYY_ASUS <tiwyy13@gmail.com>
2021-01-16 17:01:55 +0700

    delete py files

--- producer.java
+++
@@ -1,6 +0,0 @@
-Properties config = new Properties();
-config.put("client.id", 'tiw_producer');
-config.put("bootstrap.servers", "localhost:9092");
-config.put("acks", "all");
-
-producer = new KafkaProducer<String, String>(config);
